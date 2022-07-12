b7bf256622636f21968a6f8746b04d52379480d8
karishmask46 <karishmask.15999@gmail.com>
2022-07-12 14:30:11 +0530

    Employee present or absent

---
+++ Day3_EmployeeWage/Day3_EmployeeWage/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Day3_EmployeeWage
+{
+    class program
+    {
+
+        public static void Main(string[] args)
+        {
+            UC1EmployeePresent CheckEmp = new UC1EmployeePresent();
+            CheckEmp.CheckEmpPresentAbsent();
+        }
+
+
+
+    }
+}
